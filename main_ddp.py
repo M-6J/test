2a8a2223af3dff11652df1d4863a7fc79eb3ab3b
M-6J <volatus_mj@naver.com>
2024-09-12 14:24:10 +0800

    imagenet

---
+++ main_ddp.py
@@ -0,0 +1,237 @@
+import warnings
+warnings.filterwarnings(action='ignore')
+import os
+import argparse
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.cuda.amp import autocast
+import torch.cuda.amp
+from utils.utils import *
+from models.DTA_SNN import *
+import torch.multiprocessing as mp
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+
+
+
+def main_worker(rank, args):
+    seed_all(args.seed)
+    dist.init_process_group('nccl', rank = rank, world_size=args.world_size)
+
+    torch.cuda.set_device(rank)
+    torch.cuda.empty_cache()
+    args.device = 'cuda'
+    device = torch.device(args.device)
+    
+    if rank is not None:
+        print("Use GPU: {} for training".format(rank))
+
+    print('ImageNet')
+    DDP_model = dta_msresnet18(num_classes=1000, time_step=args.time_step, DTA_ON=args.DTA_ON, dvs=False)
+    DDP_model.to(device)
+    DDP_model = nn.SyncBatchNorm.convert_sync_batchnorm(DDP_model)
+    DDP_model = DDP(DDP_model, device_ids=[rank], broadcast_buffers=False)
+    
+    if rank == 0:
+        if args.resume: 
+            logger = get_logger(f'resume-ImageNet-S{args.seed}-B{args.batch_size}-T{args.time_step}-E{args.epochs}-LR{args.learning_rate}.log')
+            logger.info('start training!')
+        else:        
+            logger = get_logger(f'ImageNet-S{args.seed}-B{args.batch_size}-T{args.time_step}-E{args.epochs}-LR{args.learning_rate}.log')
+            logger.info('start training!')
+    else: logger = None
+    
+    best_acc = 0.5
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(params = DDP_model.parameters(),lr = args.lr ,momentum=0.9,weight_decay=1e-5)
+    train_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs, eta_min=0)
+    
+    training_loader, train_sampler = get_training_dataloader(
+        traindir="dataset/train",
+        num_workers=args.workers,
+        batch_size=int(args.batch_size / args.world_size),
+        shuffle=False,
+        sampler=1, # to enable sampler for DDP
+        args = args,
+        rank = rank
+    )
+    
+    test_loader = get_test_dataloader(
+        valdir="dataset/val",
+        num_workers=args.workers,
+        batch_size=int(args.batch_size / args.world_size),
+        shuffle=False,
+        args=args,
+        rank=rank
+    )
+                                               
+    scaler = torch.cuda.amp.GradScaler()
+
+    if args.resume is not None:
+        checkpoint = torch.load(args.pt_path, map_location=torch.device(rank))
+        args.start_epoch = checkpoint['epoch'] + 1
+        DDP_model.load_state_dict(checkpoint['model_state_dict']) 
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        train_scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        if rank == 0:
+            print('\nLoaded checkpoint from epoch %d.\n' % int(args.start_epoch))
+
+    for epoch in range(args.start_epoch, args.epochs):
+        
+        train_sampler.set_epoch(epoch)
+        train(device, rank, DDP_model, training_loader, criterion, optimizer, args, scaler, logger, epoch)
+        train_scheduler.step()
+        
+        if rank == 0:
+            curr_acc = eval_training(device, test_loader, DDP_model, criterion, args, logger, epoch)
+        
+            if best_acc < curr_acc:
+                best_acc = curr_acc
+                checkpoint = {'epoch': epoch+1,
+                          'model_state_dict': DDP_model.state_dict(),
+                          'optimizer_state_dict': optimizer.state_dict(),
+                          'scheduler_state_dict': train_scheduler.state_dict()}
+                torch.save(DDP_model.module.state_dict(), f'ImageNet-S{args.seed}-B{args.batch_size}-T{args.time_step}-E{args.epochs}-LR{args.learning_rate}.pth.tar')
+
+            logger.info('Best Test acc={:.3f}'.format(best_acc ))
+            logger.info('\n')
+        else: 
+            pass
+
+
+def train(device, rank, model, training_loader, criterion, optimizer, args, scaler, logger, epoch):
+    running_loss = 0
+
+    model.train()
+    correct = 0.0
+    num_sample = 0
+    
+    for batch_index, (images, labels) in enumerate(training_loader):
+        labels = labels.to(device, non_blocking=True)
+        images = images.to(device, non_blocking=True)
+        num_sample += images.size()[0]
+        optimizer.zero_grad()
+
+        with autocast():
+            outputs = model(images)
+            mean_out = outputs.mean(1)
+            loss = criterion(mean_out, labels)
+            _, predicted = mean_out.cpu().max(1)
+            correct += float(predicted.eq(labels.cpu()).sum().item())
+            running_loss += loss.item()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+    if rank == 0:
+        acc = correct/num_sample* 100
+        logger.info('Epoch:[{}/{}]\t loss={:.5f}\t acc={:.3f}'.format(epoch+1, args.epochs, running_loss, acc))
+
+
+
+@torch.no_grad()
+def eval_training(device, test_loader, model, criterion, args, logger, epoch):
+
+    model.eval()
+
+    running_loss = 0.0
+    correct = 0.0
+    real_batch = 0
+    with torch.no_grad():
+        for (images, labels) in test_loader:
+            real_batch += images.size()[0]
+
+            images = images.to(device,non_blocking=True)
+            labels = labels.to(device,non_blocking=True)
+
+            outputs = model(images)
+            mean_out = outputs.mean(1)
+            
+            loss = criterion(mean_out, labels)
+            criterion(mean_out,labels)
+            running_loss += loss.item()
+
+            _, predicted = mean_out.cpu().max(1)
+            correct += float(predicted.eq(labels.cpu()).sum().item())
+
+    acc = correct / len(test_loader.dataset) * 100
+
+    logger.info('Epoch:[{}/{}]\t loss={:.5f}\t acc={:.3f}'.format(epoch+1, args.epochs, running_loss, acc))
+    
+    return acc
+
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--DTA_ON',
+                    default=True,
+                    type=bool,
+                    help='using DTA')
+    
+    parser.add_argument('--start_epoch',
+                        default=0,
+                        type=int,
+                        metavar='N',
+                        help='manual epoch number')
+
+    parser.add_argument('--batch_size',
+                        default=128,
+                        type=int,
+                        metavar='N')
+
+    parser.add_argument('--learning_rate',
+                        default=0.1,
+                        type=float,
+                        metavar='LR',
+                        help='initial learning rate')
+
+    parser.add_argument('--seed',
+                        default=42,
+                        type=int,
+                        help='seed for initializing training')
+
+    parser.add_argument('--time_step',
+                        default=6,
+                        type=int,
+                        metavar='N',
+                        help='snn simulation time steps (default: 6)')
+    
+    parser.add_argument('--workers',
+                        default=16,
+                        type=int,
+                        metavar='N',
+                        help='number of data loading workers (default: 16)')
+    
+    parser.add_argument('--epochs',
+                        default=200,
+                        type=int,
+                        metavar='N',
+                        help='number of total epochs to run')
+    
+    parser.add_argument('--world_size', 
+                        default=4, 
+                        type=int,
+                        help='world size')
+    
+    parser.add_argument('--resume', 
+                        default=False, 
+                        type=bool,
+                        help = 'O X pre-trained')
+    
+    parser.add_argument('--pt_path',
+                        type=str,
+                        help = 'resume path')
+
+    args = parser.parse_args()
+
+    mp.spawn(main_worker,nprocs=args.world_size,args=(args,),join=True)
+
+
+    
+    
--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,12 @@ import random
 import numpy as np 
 import torch 
 import math 
+import torchvision.datasets as datasets
+import albumentations as A
+from PIL import Image
+from torch.utils.data import DataLoader
+from albumentations.pytorch import ToTensorV2
+from torch.utils.data.distributed import DistributedSampler
 
 def get_logger(filename, verbosity=1, name=None):
     level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
@@ -72,3 +78,63 @@ def split_to_train_test_set(train_ratio: float, origin_dataset: torch.utils.data
 
     return torch.utils.data.Subset(origin_dataset, train_idx), torch.utils.data.Subset(origin_dataset, test_idx)
     
+
+#for ImageNet
+class Transform():
+     def __init__(self,transform):
+        self.transform=transform
+     def __call__(self,image):
+        return self.transform(image=image)["image"]
+     
+def open_img(img_path):
+     img=Image.open(img_path).convert('RGB')
+     return np.array(img)
+
+def get_training_dataloader(traindir, sampler=None, batch_size=16, num_workers=2, shuffle=True, args=None, rank=None):
+
+    train_transforms = A.Compose([
+    A.RandomResizedCrop(224, 224),
+    A.HorizontalFlip(),
+    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ToTensorV2()])
+
+    ImageNet_training = datasets.ImageFolder(traindir, transform=Transform(train_transforms), loader=open_img)
+
+
+    ImageNet_train_sampler = DistributedSampler(dataset=ImageNet_training, seed=args.seed, num_replicas=args.world_size, rank=rank)
+    
+    ImageNet_training_loader = DataLoader(
+        ImageNet_training,
+        shuffle=False,
+        num_workers=num_workers,
+        batch_size=batch_size,
+        pin_memory=True,
+        sampler=ImageNet_train_sampler
+        )
+   
+    return ImageNet_training_loader, ImageNet_train_sampler
+
+
+def get_test_dataloader(valdir, sampler=None, batch_size=16, num_workers=2, shuffle=True, args=None, rank=None):
+    val_transforms = A.Compose([
+        A.Resize(256, 256),
+        A.CenterCrop(224, 224),
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2()
+    ])
+    
+    ImageNet_test = datasets.ImageFolder(
+        valdir,
+        transform=Transform(val_transforms),
+        loader=open_img
+    )
+    
+    ImageNet_test_loader = DataLoader(
+        ImageNet_test,
+        shuffle=False,
+        num_workers=num_workers,
+        batch_size=batch_size,
+        pin_memory=True,
+    )
+    
+    return ImageNet_test_loader
